3cdfcdd6d93d0c097c00d67d4830e56d98da127a
Shelby M <Shelby M@Shelby-PC>
2018-10-27 12:33:23 -0600

    Minor edits and other placeholder images

--- CSCI_136_Lab_7/src/Driver.java
+++ CSCI_136_Lab_7/src/Driver.java
@@ -8,22 +8,21 @@ import javafx.stage.Stage;
 public class Driver extends Application{
 
 	public static void main(String[] args) {
-		launch(args); //will launch the page (probably wouldn't run code w/o it
+		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		double x = 100; //x-coordinate of where the text will start
-		double y = 200; //y-coordinate of where the text will start
+		int x = 400; //x-coordinate of where the image will start
+		int y = 400; //y-coordinate of where the image will start
 		String words = "hi";
 		Text message = new Text(x, y, words); //input numbers and string for position and text
-		Group textGroup = new Group(message); //the group of objects that will be added to the window
-		int width = 500;
-		int height = 800;
-		Scene background = new Scene(textGroup, width, height, Color.AZURE);
-		//Sets up instructions for: adding the group of objects, setting up window size, and picking window background color
-		primaryStage.setTitle("Title in Bar"); //title of window in the window's bar
+		
+		Group myGroup = new Group(message); //the group of objects that will be added to the window
+		Scene background = new Scene(myGroup, 800, 600, Color.WHITE);
+		
+		primaryStage.setTitle("Game"); //title of window in the window's bar
 		primaryStage.setScene(background); //implements the "scene" (objects/background)
-		primaryStage.show(); //shows the window (would probably not appear w/o it
+		primaryStage.show(); //shows the window (would probably not appear w/o it)
 	}
 }
